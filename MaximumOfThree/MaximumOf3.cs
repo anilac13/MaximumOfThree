62b57484de49cfb0ea3fff59c874cea17e3cc0b3
anilac13 <anil4u2496@gmail.com>
2023-05-13 20:34:05 +0530

    [Anil] Add:Given 3 integers, find the maximum number

---
+++ MaximumOfThree/MaximumOf3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumOfThree
+{
+    public class MaximumOf3
+    {
+        public int FindMaxOfIntegers(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return firstNum;
+        }
+    }
+}
--- MaximumOfThree/Program.cs
+++ MaximumOfThree/Program.cs
@@ -5,7 +5,15 @@ namespace MaximumOfThree
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter 1st number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            MaximumOf3 maximum = new MaximumOf3();
+            int val = maximum.FindMaxOfIntegers(num1, num2, num3);
+            Console.WriteLine("Maximum number is: {0}", val);
         }
     }
 }
