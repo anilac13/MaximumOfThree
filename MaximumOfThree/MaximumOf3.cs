dbe9592d1ff6af8bb2d2fe55fd4fb6fcbcd4579b
anilac13 <anil4u2496@gmail.com>
2023-05-14 11:01:34 +0530

    "[Anil] Add:Refactor 3 to one Generic method and find maximum"
    This reverts commit 085e66b6f573b0d16827ba53366527d26b44d61c.

--- MaximumOfThree/MaximumOf3.cs
+++ MaximumOfThree/MaximumOf3.cs
@@ -6,73 +6,73 @@ using System.Threading.Tasks;
 
 namespace MaximumOfThree
 {
-    public class MaximumOf3<T> where T:IComparable
+    public class MaximumOf3
     {
-        public T FindMaxOfThree(T first, T second, T third)
+        public int FindMaxOfIntegers(int firstNum, int secondNum, int thirdNum)
         {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
-                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
-                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return firstNum;
+        }
+        public double FindMaxOfDouble(double firstNum, double secondNum, double thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 ||
+                firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 ||
+                secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 ||
+                thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return firstNum;
+        }
+        public string FindMaxOfString(string first, string second, string third)
+        {
+            if (first.Length.CompareTo(second.Length) > 0 && first.Length.CompareTo(third.Length) > 0 ||
+                first.Length.CompareTo(second.Length) >= 0 && first.Length.CompareTo(third.Length) > 0 ||
+                first.Length.CompareTo(second.Length) > 0 && first.Length.CompareTo(third.Length) >= 0)
             {
                 return first;
             }
-            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
-                second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
-                second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            if (second.Length.CompareTo(first.Length) > 0 && first.Length.CompareTo(third.Length) > 0 ||
+                second.Length.CompareTo(first.Length) >= 0 && first.Length.CompareTo(third.Length) > 0 ||
+                second.Length.CompareTo(first.Length) > 0 && first.Length.CompareTo(third.Length) >= 0)
             {
                 return second;
             }
-            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
-                third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
-                third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            if (third.Length.CompareTo(first.Length) > 0 && third.Length.CompareTo(second.Length) > 0 ||
+                third.Length.CompareTo(first.Length) >= 0 && third.Length.CompareTo(second.Length) > 0 ||
+                third.Length.CompareTo(first.Length) > 0 && third.Length.CompareTo(second.Length) >= 0)
             {
                 return third;
             }
             return first;
         }
-        //public double FindMaxOfDouble(double firstNum, double secondNum, double thirdNum)
-        //{
-        //    if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 ||
-        //        firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 ||
-        //        firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
-        //    {
-        //        return firstNum;
-        //    }
-        //    if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 ||
-        //        secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 ||
-        //        secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
-        //    {
-        //        return secondNum;
-        //    }
-        //    if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 ||
-        //        thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 ||
-        //        thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
-        //    {
-        //        return thirdNum;
-        //    }
-        //    return firstNum;
-        //}
-        //public string FindMaxOfString(string first, string second, string third)
-        //{
-        //    if (first.Length.CompareTo(second.Length) > 0 && first.Length.CompareTo(third.Length) > 0 ||
-        //        first.Length.CompareTo(second.Length) >= 0 && first.Length.CompareTo(third.Length) > 0 ||
-        //        first.Length.CompareTo(second.Length) > 0 && first.Length.CompareTo(third.Length) >= 0)
-        //    {
-        //        return first;
-        //    }
-        //    if (second.Length.CompareTo(first.Length) > 0 && first.Length.CompareTo(third.Length) > 0 ||
-        //        second.Length.CompareTo(first.Length) >= 0 && first.Length.CompareTo(third.Length) > 0 ||
-        //        second.Length.CompareTo(first.Length) > 0 && first.Length.CompareTo(third.Length) >= 0)
-        //    {
-        //        return second;
-        //    }
-        //    if (third.Length.CompareTo(first.Length) > 0 && third.Length.CompareTo(second.Length) > 0 ||
-        //        third.Length.CompareTo(first.Length) >= 0 && third.Length.CompareTo(second.Length) > 0 ||
-        //        third.Length.CompareTo(first.Length) > 0 && third.Length.CompareTo(second.Length) >= 0)
-        //    {
-        //        return third;
-        //    }
-        //    return first;
-        //}
     }
 }
--- MaximumOfThree/Program.cs
+++ MaximumOfThree/Program.cs
@@ -5,51 +5,15 @@ namespace MaximumOfThree
     {
         static void Main(string[] args)
         {
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("\nChoose option to perform \n1.Maximum of 3 Inetegers \n2.Maximum of 3 Decimal numbers \n3.Maximum of 3 strings \n4.Exit");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        Console.WriteLine("Enter 1st number");
-                        int a = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter 2nd number");
-                        int b = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter 3rd number");
-                        int c = Convert.ToInt32(Console.ReadLine());
-                        MaximumOf3<int> maximum = new MaximumOf3<int>();
-                        int val = maximum.FindMaxOfThree(a, b, c);
-                        Console.WriteLine("Maximum number is: {0}", val);
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter 1st number");
-                        double num1 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Enter 2nd number");
-                        double num2 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Enter 3rd number");
-                        double num3 = Convert.ToDouble(Console.ReadLine());
-                        MaximumOf3<double> max = new MaximumOf3<double>();
-                        double value = max.FindMaxOfThree(num1, num2, num3);
-                        Console.WriteLine("Maximum number is: {0}", value);
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter 1st string");
-                        string str1 = Console.ReadLine();
-                        Console.WriteLine("Enter 2nd string");
-                        string str2 = Console.ReadLine();
-                        Console.WriteLine("Enter 3rd string");
-                        string str3 = Console.ReadLine();
-                        MaximumOf3<string> maximumOf3 = new MaximumOf3<string>();
-                        string result = maximumOf3.FindMaxOfThree(str1, str2, str3);
-                        Console.WriteLine("Maximum string is: {0}", result);
-                        break;
-                    case 4:
-                        flag = false;
-                        break;
-                }
-            }
+            Console.WriteLine("Enter 1st string");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter 2nd string");
+            string b = Console.ReadLine();
+            Console.WriteLine("Enter 3rd string");
+            string c = Console.ReadLine();
+            MaximumOf3 maximum = new MaximumOf3();
+            string val = maximum.FindMaxOfString(a, b, c);
+            Console.WriteLine("Maximum string is: {0}", val);
         }
     }
 }
